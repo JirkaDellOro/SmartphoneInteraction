7aad4596ba7c90170578e6ac429aa8bb948f3a68
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-08-14 09:50:25 +0200

    log pointer event

--- Build/Build.js
+++ Build/Build.js
@@ -7,7 +7,12 @@ var SmartphoneInteraction;
         ƒ.DebugTextArea.textArea = document.querySelector("textarea");
         // let targets: ƒ.DebugTarget[] = [ƒ.DebugConsole, ƒ.DebugTextArea, ƒ.DebugAlert];
         ƒ.Debug.setFilter(ƒ.DebugTextArea, ƒ.DEBUG_FILTER.ALL);
-        ƒ.Debug.setFilter(ƒ.DebugAlert, ƒ.DEBUG_FILTER.ALL);
+        // ƒ.Debug.setFilter(ƒ.DebugAlert, ƒ.DEBUG_FILTER.ALL);
         ƒ.Debug.log("Hallo");
+        document.addEventListener("pointerdown", hndPointerEvent);
+        document.addEventListener("pointerup", hndPointerEvent);
+    }
+    function hndPointerEvent(_event) {
+        ƒ.Debug.log(_event.pointerType, _event.pointerId);
     }
 })(SmartphoneInteraction || (SmartphoneInteraction = {}));
